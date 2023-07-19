a6c0731f571d8e4d473ccc99041b965e57e4152d
JoshiNagendra <joshichennamsetti999@gmail.com>
2023-07-19 23:12:05 +0530

    Sample1 is modified and u method added

--- Sample1.java
+++ Sample1.java
@@ -1,19 +1,9 @@
 package abstraction.part1;
-class Joshi{
-	public Joshi() {
-		System.out.println("Constructor Joshi");
-	}
-}
-class Vamsi extends Joshi{
-	public Vamsi() {
-		System.out.println("Vamsi Constructor");
-	}
-	
-}
-
 public class Sample1 {
 	public static void main(String[] args) {
-		Vamsi v= new Vamsi();
+		System.out.println("newly added this code U");
+
+
 	}
 
 }
